ba07fde4ad9b642f66b555e333b618bf98210c22
esaaprillia <esaapriliasalsabila@gmail.com>
2026-05-22 11:18:24 +0800

    Update resource_usage_utils.cpp

--- lib/support/resource_usage/resource_usage_utils.cpp
+++ lib/support/resource_usage/resource_usage_utils.cpp
@@ -21,7 +21,7 @@ static cpu_snapshot to_snapshot(const ::rusage& rusg)
 expected<cpu_snapshot, int> resource_usage_utils::cpu_usage_now(rusage_measurement_type type)
 {
   ::rusage ret;
-  if (::getrusage(static_cast<__rusage_who_t>(type), &ret) == 0) {
+  if (::getrusage(static_cast<int>(type), &ret) == 0) {
     return to_snapshot(ret);
   }
   return make_unexpected(errno);
